175f25eae2b12b0cdac292186093dc9f1ba3f347
RouRai <k1215050@students.katyisd.org>
2023-04-25 16:27:22 -0500

    added scatterplot

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+dataframe = pd.read_csv("variables_check.csv")
 
+plt.scatter(dataframe["Life expectancy "], dataframe["Adult Mortality"])
+plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
