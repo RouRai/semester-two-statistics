1fc2a2ad1c97599dae589b8c9b3cf32dda0c2930
RouRai <k1215050@students.katyisd.org>
2023-05-06 12:48:40 -0500

    better plot

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 dataframe = pd.read_csv("variables_check.csv")
 dataframe = pd.DataFrame(dataframe, columns=['Life Expectancy', 'Adult Mortality'])
 
 print(dataframe.sample(n=18))
 
-plt.scatter(x=dataframe['Life Expectancy'], y=dataframe['Adult Mortality'])
+plt.scatter(x=dataframe['Life Expectancy'], y=dataframe['Adult Mortality'], c=dataframe['Life Expectancy'])
+plt.title("Life Expectancy vs. Adult Mortality")
+plt.xlabel("Life Expectancy (years)")
+plt.ylabel("Adult Mortality (per 100,000)")
 plt.show()
