11d3efdbe72a324c6b11d8681aa53a45c857ad8f
RouRai <k1215050@students.katyisd.org>
2023-05-07 14:14:36 -0500

    thing lmao

--- sampleGraphing.py
+++ sampleGraphing.py
@@ -1,6 +1,10 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 dataframe = pd.read_csv("variables_check.csv")
 dataframe = pd.DataFrame(dataframe, columns=['Life Expectancy', 'Adult Mortality'])
 
-print(dataframe.sample(n=18))
+subset = dataframe.sample(n=18)
+
+plt.scatter(x=subset['Life Expectancy'], y=subset['Adult Mortality'], c=subset['Life Expectancy'])
+plt.show()
